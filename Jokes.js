7f59f94fa6054167d9cc15c0fd7f5750f9026f6e
Justin Thomas <ThomasCJustin@gmail.com>
2017-11-07 14:37:48 -0500

    Minor format update

--- Jokes.js
+++ Jokes.js
@@ -1,6 +1,8 @@
 var jokeContainer = document.getElementById("joke-here");
 var btn = document.getElementById("btn");
 var tweet = document.getElementById("twitter");
+var setName = document.getElementById("setName");
+var customJoke = document.getElementById("custom");
 
 btn.addEventListener("click", function () {
   var request = new XMLHttpRequest();
@@ -8,36 +10,32 @@ btn.addEventListener("click", function () {
   request.onload = function () {
     var theData = JSON.parse(request.responseText);
     renderHTML(theData);
-    
-    /*  The delivered joke may have encoding issues 
-        when tweeted. 
-        
-        This part is for Twitter button functionality.   
+
+    /*  The delivered joke may have encoding issues
+        when tweeted.
+
+        This part is for Twitter button functionality.
     */
-   
+
     var chuckQuote = theData.value.joke;
-   
+
     if (chuckQuote.length > 125) {
       chuckQuote = chuckQuote.slice(0, 120) + "...";
     }
-   
+
     chuckQuote = encodeURIComponent(chuckQuote);
-    
+
     tweet.setAttribute('href', "https://twitter.com/intent/tweet?hashtags=ChuckNorris&text=" + chuckQuote + " ");
-    
+
   };
   request.send();
-    
-});
 
-var setName = document.getElementById("setName");
+});
 
 setName.addEventListener("click", function () {
   firstName = prompt("Enter your first name: ");
   lastName = prompt("Enter your last name: ");
 });
-
-var customJoke = document.getElementById("custom");
 
 customJoke.addEventListener("click", function () {
   var request = new XMLHttpRequest();
@@ -45,34 +43,33 @@ customJoke.addEventListener("click", function () {
   request.onload = function () {
     var theData = JSON.parse(request.responseText);
     renderHTML(theData);
-    
-    /*  The delivered joke may have encoding issues 
-        when tweeted. 
-        
-        This part is for Twitter button functionality.   
+
+    /*  The delivered joke may have encoding issues
+        when tweeted.
+
+        This part is for Twitter button functionality.
     */
-   
+
     var chuckQuote = theData.value.joke;
-   
+
     if (chuckQuote.length > 125) {
       chuckQuote = chuckQuote.slice(0, 120) + "...";
     }
-   
+
     chuckQuote = encodeURIComponent(chuckQuote);
-    
+
     tweet.setAttribute('href', "https://twitter.com/intent/tweet?hashtags=ChuckNorris&text=" + chuckQuote);
-    
+
   };
   request.send();
-    
-});
 
+});
 
 function renderHTML(data) {
   jokeContainer.innerHTML = "";
   var htmlString = "";
-  
+
   htmlString += "<p>" + data.value.joke + "</p>";
-  
+
   jokeContainer.insertAdjacentHTML('beforeend', htmlString);
 }
